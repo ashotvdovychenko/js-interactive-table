491265b440f222bc163a4a360d04c9be5d887417
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-10-19 01:17:50 +0300

    Table implementation

---
+++ scripts/form.js
@@ -0,0 +1,103 @@
+
+class SmartControl {
+    #controlRef
+
+    constructor(selector, initialValue, additionalValidators = []) {
+        this.#controlRef = document.querySelector(selector);
+
+        this.writeValue(initialValue);
+
+        additionalValidators.forEach(additionalValidator => additionalValidator(this.#controlRef))
+    }
+
+    writeValue(value) {
+        this.#controlRef.setAttribute("value", value);
+    }
+
+    get value() {
+        return this.#controlRef.value;
+    }
+
+    get control() {
+        return this.#controlRef;
+    }
+}
+class SmartPhoneControl extends SmartControl{
+    #controlMask;
+
+    constructor(selector, mask, additionalValidators = []) {
+        super(selector, mask, additionalValidators)
+
+        this.#listenControlWriteValue();
+    }
+
+    #listenControlWriteValue() {
+        this.control.addEventListener('keydown', (event) => {
+            event.preventDefault();
+
+            const { target, key, keyCode } = event;
+            const inputValue = target.value;
+            const isNumber = keyCode > 47 && keyCode < 58;
+            const isBackspace = keyCode === 8;
+
+            if (isNumber) {
+                this.control.setAttribute('value', this.#getPhoneMaskWithAddedNumber(inputValue, key));
+            }
+
+            if (isBackspace) {
+                this.control.setAttribute('value', this.#getPhoneMaskWithRemovedNumber(inputValue, this.#controlMask));
+            }
+        }, true)
+    }
+
+    #getPhoneMaskWithAddedNumber(value, key) {
+        const valueArray = [...value];
+        const currentIndex = valueArray.findIndex(position => position === '_')
+
+        valueArray[currentIndex] = key;
+
+        return valueArray.join('');
+    }
+
+    #getPhoneMaskWithRemovedNumber(value, initialMask) {
+        const valueArray = [...value];
+        const currentIndex = valueArray.length - 1 - [...valueArray].reverse().findIndex(value => Number.isInteger(+value) && value !== ' ');
+        const removableIndex = currentIndex === -1 ? value.length - 1 : currentIndex;
+
+        valueArray[removableIndex] = initialMask[removableIndex];
+
+        return valueArray.join('');
+    }
+}
+
+class SmartForm {
+    #formRef;
+    #formControlRefs;
+    #submitAction;
+
+    constructor(selector, submitAction) {
+        this.#formRef = document.querySelector(selector);
+        this.#formControlRefs = [...this.#formRef.querySelectorAll('input:not([type="submit"])')];
+        this.#submitAction = submitAction;
+
+        this.#handleFormSubmit();
+    }
+
+    #handleFormSubmit() {
+        this.#formRef.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            const data = this.#formControlRefs
+                .filter((control) => control.type === 'radio' ? control.checked : true)
+                .reduce((acc, control) => {
+                    const propertyName = control.attributes.getNamedItem('name').value;
+
+                    acc[propertyName] = control.value;
+
+                    return acc;
+                }, { id: new Date().getTime() })
+
+            this.#submitAction(data);
+        })
+    }
+}
---
+++ scripts/table.js
@@ -0,0 +1,220 @@
+// TODO: Extract selection logic in separate child class to keep Single Responsibility Principle for SmartTable and avoid GOD object;
+class SmartTable {
+    #tableContainerRef;
+    #tableHeaderRef;
+    #tableBodyRef;
+    #colDef;
+    #selectable;
+
+    constructor(selector, colDef, selectable = true) {
+        this.#tableContainerRef = document.querySelector(selector);
+        this.#colDef = colDef;
+        this.#selectable = selectable;
+
+        this.#tableHeaderRef = this.#getTableHeaderRef(colDef, selectable);
+        this.#tableBodyRef = this.#getTableBodyRef();
+
+        this.#drawTable(this.#tableBodyRef, this.#tableHeaderRef);
+    }
+
+    get rowData() {
+        return [...this.#tableBodyRef.children].map(row => {
+            return [...row.children]
+                .slice(this.#selectable ? 1 : 0)
+                .reduce((acc, col, i) => {
+                    acc[this.#colDef[i].propertyName] = col.textContent;
+
+                    return acc;
+                }, {});
+        });
+    }
+
+    get getSelectedRows() {
+        if (this.#selectable) {
+            return [...this.#tableBodyRef.children]
+                .filter(rowRef => {
+                    const checkboxRef = rowRef.querySelector('input[type="checkbox"]');
+
+                    return checkboxRef.checked;
+                })
+                .map(rowRef => this.rowData.find(row => row.id === rowRef.dataset.id));
+        }
+
+        return []
+    }
+
+    setRowData(data) {
+        for (let row of data) {
+            const rowRef = document.createElement('tr');
+
+            rowRef.dataset.id = row.id;
+
+            if (this.#selectable) {
+                const columnRef = document.createElement('th')
+                const checkboxRef = document.createElement('input');
+
+                checkboxRef.type = 'checkbox';
+                checkboxRef.classList.add('form-check-input');
+
+                columnRef.appendChild(checkboxRef);
+                rowRef.appendChild(columnRef);
+            }
+
+            for (let col of this.#colDef) {
+                const columnRef = document.createElement('td');
+
+                columnRef.textContent = row[col.propertyName];
+
+                rowRef.appendChild(columnRef);
+            }
+
+            this.#tableBodyRef.appendChild(rowRef);
+        }
+    }
+
+    addRow(row) {
+        const rowRef = document.createElement('tr');
+        rowRef.dataset.id = row.id;
+
+        if (this.#selectable) {
+            const columnRef = this.#getSelectControlColRef();
+
+            rowRef.appendChild(columnRef);
+        }
+
+        for (let col of this.#colDef) {
+            const columnRef = document.createElement('td');
+
+            columnRef.textContent = row[col.propertyName];
+
+            rowRef.appendChild(columnRef);
+        }
+
+        this.#tableBodyRef.appendChild(rowRef);
+    }
+
+    removeRow(id) {
+        this.#tableBodyRef.querySelector(`tr[data-id='${id}']`).remove();
+    }
+
+    #getTableBodyRef() {
+        return document.createElement('tbody');
+    }
+
+    #getTableHeaderRef(colDef, selectable) {
+        const theadRef = document.createElement('thead');
+        const rowRef = document.createElement('tr')
+
+        if (selectable) {
+            const columnRef = document.createElement('th')
+            const checkboxRef = document.createElement('input');
+
+            checkboxRef.type = 'checkbox';
+            checkboxRef.classList.add('form-check-input');
+
+            columnRef.appendChild(checkboxRef);
+            rowRef.appendChild(columnRef);
+
+            this.#handleSelectAllControlChange(checkboxRef);
+        }
+
+        for (let col of colDef) {
+            const columnRef = document.createElement('th')
+            columnRef.textContent = col.headerText;
+            rowRef.appendChild(columnRef)
+        }
+
+        theadRef.appendChild(rowRef);
+
+        return theadRef;
+    }
+
+    #getSelectControlColRef() {
+        const columnRef = document.createElement('th');
+        const checkboxRef = document.createElement('input');
+
+        checkboxRef.type = 'checkbox';
+        checkboxRef.classList.add('form-check-input');
+
+        columnRef.appendChild(checkboxRef);
+
+        return columnRef;
+    }
+
+    #handleSelectAllControlChange(controlRef) {
+        controlRef.addEventListener('change', (event) => {
+            [...this.#tableBodyRef.children].forEach(rowRef => {
+                const checkboxRef = rowRef.querySelector('input[type="checkbox"]');
+
+                checkboxRef.checked = event.target.checked;
+            })
+        })
+    }
+
+    #drawTable(bodyRef, headerRef) {
+        this.#tableContainerRef.innerHTML = '';
+        this.#tableContainerRef.appendChild(bodyRef);
+        this.#tableContainerRef.appendChild(headerRef);
+    }
+}
+
+class SmartTableWithControls extends SmartTable {
+    #deleteButtonRef;
+    #duplicateButtonRef;
+
+    constructor(selector, colDef, selectable = true, deleteButtonSelector, duplicateButtonSelector) {
+        super(selector, colDef, selectable);
+        this.#deleteButtonRef = document.querySelector(deleteButtonSelector);
+        this.#duplicateButtonRef = document.querySelector(duplicateButtonSelector);
+
+        this.#handleDeleteButtonClick();
+        this.#handleDuplicateButtonClick();
+    }
+
+    #handleDeleteButtonClick() {
+        this.#deleteButtonRef.addEventListener('click', () => {
+            this.getSelectedRows.forEach(selectedRow => this.removeRow(selectedRow.id))
+        })
+    }
+
+    #handleDuplicateButtonClick() {
+        this.#duplicateButtonRef.addEventListener('click', () => {
+            this.getSelectedRows.forEach(selectedRow => {
+                const duplicatedRow = { ...selectedRow };
+
+                duplicatedRow.id = new Date().getTime();
+
+                this.addRow(duplicatedRow);
+            })
+        })
+    }
+}
+
+// tableSelectAllControl.addEventListener('click', (event) => {
+//     const tableCheckboxRefs = document.querySelectorAll('.table-status-checkbox');
+//
+//     tableCheckboxRefs.forEach(tableCheckboxRef => {
+//         tableCheckboxRef.checked = event.target.checked;
+//     });
+// })
+
+
+// function registerCheckbox() {
+//     const allSelectControls = document.querySelectorAll('.table-status-checkbox');
+//
+//     allSelectControls.forEach(control => control.addEventListener('click', () => {
+//         const isAllSelected = [...allSelectControls].every(control => control.checked)
+//         const isAllDeselected = [...allSelectControls].every(control => !control.checked);
+//
+//         if (tableSelectAllControl.checked && isAllDeselected) {
+//             tableSelectAllControl.checked = false;
+//             tableSelectAllControl.indeterminate = false;
+//         } else if (isAllSelected) {
+//             tableSelectAllControl.checked = true;
+//             tableSelectAllControl.indeterminate = false;
+//         } else {
+//             tableSelectAllControl.indeterminate = true;
+//         }
+//     }))
+// }
+
